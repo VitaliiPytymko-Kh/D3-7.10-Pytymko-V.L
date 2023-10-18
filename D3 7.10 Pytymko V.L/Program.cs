6e4263ff8c4b982ef26cb726f1ce2ff794c6c23a
VitaliiPytymko <vitaliipytymko@gmail.com>
2023-10-18 22:49:28 +0300

    Додано клас Store

--- D3 7.10 Pytymko V.L/Program.cs
+++ D3 7.10 Pytymko V.L/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static D3_7._10_Pytymko_V.L.Class1;
+using static D3_7._10_Pytymko_V.L.Task2;
 
 namespace D3_7._10_Pytymko_V.L
 {
@@ -12,21 +13,38 @@ namespace D3_7._10_Pytymko_V.L
         static void Main(string[] args)
         {
             //Task1
-            Journal MyJournal = new Journal("dtryh", 2000, "rtyrty", "1111", "sdtyrty",200);
-            MyJournal.PrintJournal();
+            //Journal MyJournal = new Journal("dtryh", 2000, "rtyrty", "1111", "sdtyrty",200);
+            //MyJournal.PrintJournal();
 
-            MyJournal += 11;
-            MyJournal.PrintJournal();
+            //MyJournal += 11;
+            //MyJournal.PrintJournal();
 
-            MyJournal -= 25;
-            MyJournal.PrintJournal();
+            //MyJournal -= 25;
+            //MyJournal.PrintJournal();
 
-            Journal MyJournal1 = new Journal("dtryh", 2000, "rtyrty", "1111", "sdtyrty", 200);
-            Console.WriteLine($" Equals (MyJournal,MyJournal1)={Equals(MyJournal.GetPersonal(),MyJournal1.GetPersonal())}");
-            Journal A = new Journal("fgh",6666,"hjkjh","566574-567465","jkljk@fgjh",186);
-            A.PrintJournal();
-            Console.WriteLine($" Equals (MyJournal,A)={Equals(MyJournal.GetPersonal(), A.GetPersonal())}");
-            
+            //Journal MyJournal1 = new Journal("dtryh", 2000, "rtyrty", "1111", "sdtyrty", 200);
+            //Console.WriteLine($" Equals (MyJournal,MyJournal1)={Equals(MyJournal.GetPersonal(),MyJournal1.GetPersonal())}");
+            //Journal A = new Journal("fgh",6666,"hjkjh","566574-567465","jkljk@fgjh",186);
+            //A.PrintJournal();
+            //Console.WriteLine($" Equals (MyJournal,A)={Equals(MyJournal.GetPersonal(), A.GetPersonal())}");
+
+            //Task2
+            Store myStore = new Store(" БТА", "Адреса магазину - Ненацьк ", "БАвовнятко ", "Телефон: Mi", " Bavovna@nenack.com");
+
+            myStore.ShowStore();
+
+            Console.WriteLine();
+
+            myStore.SetNameS($"Нова назва ");
+
+            myStore.ShowStore();
+
+
+
+
+
+            Console.WriteLine();
+
         }
     }
 }
---
+++ D3 7.10 Pytymko V.L/Task2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3_7._10_Pytymko_V.L
+{
+    internal class Task2
+    {
+        public class Store
+        {
+            private string _nameS;
+            private string _adress;
+            private string _profildescriptionS;
+            private string _phones;
+            private string _emailS;
+
+            public Store(string nameS, string adress, string profildescriptionS, string phones, string emailS)
+            {
+                _nameS = nameS;
+                _adress = adress;
+                _profildescriptionS = profildescriptionS;
+                _phones = phones;
+                _emailS = emailS;
+            }
+
+            public string NameS { get { return _nameS; } }
+            public string Adress { get { return _adress; } }
+            public string Profil { get { return _profildescriptionS; } }
+            public string Phones { get { return _phones; } }
+
+            public string EmaiS { get { return _emailS; } }
+
+            public void SetNameS(string newNames) { _nameS = newNames; }
+            public void SetAdress(string newAdress) { _adress = newAdress; }
+            public void SetProfil(string newProfil) { _profildescriptionS = newProfil; }
+            public void SetPhones(string newPhonees) { _phones = newPhonees; }
+            public void SetEmail(string newEmails) { _emailS = newEmails; }
+
+            public void InputDataStore()
+            {
+                Console.Write("Введіть назву магазину: ");
+                _nameS = Console.ReadLine();
+
+                Console.Write("Введіть адресу магазину: ");
+                _adress = Console.ReadLine();
+
+                Console.Write("Введіть опис профілю магазину: ");
+                _profildescriptionS = Console.ReadLine();
+
+                Console.Write("Введіть контактний телефон: ");
+                _phones = Console.ReadLine();
+
+                Console.Write("Введіть email: ");
+                _emailS = Console.ReadLine();
+            }
+
+            public void ShowStore()
+            {
+                Console.WriteLine("Інформація про магазин:");
+                Console.WriteLine("" + _nameS);
+                Console.WriteLine("" + _adress);
+                Console.WriteLine("" + _profildescriptionS);
+                Console.WriteLine("" + _phones);
+                Console.WriteLine("" + _emailS);
+
+            }
+        }
+    }
+}
